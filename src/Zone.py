2cb4824e523007ba26ef871c369ef50435c691fd
luccribe <lunnarramalho@outlook.com>
2026-04-21 16:05:48 -0300

    fix: removed visited attribute because it won't work with multiple algorithms traversing through zones simultaneously

--- src/Zone.py
+++ src/Zone.py
@@ -1,26 +1,24 @@
 class Zone:
     def __init__(self, name: str, zone_type: str,
                  pos: tuple[int, int],
-                 connections: list[Connection] = [],
                  is_start: bool = False,
                  is_end: bool = False,
                  max_drones: int = 1,
-                 color: str | None = None,
-                 visited: bool = False) -> None:
+                 color: str | None = None) -> None:
         self.name = name
         self.zone_type = zone_type
         self.pos = pos
-        self.connections = connections
         self.is_start = is_start
         self.is_end = is_end
         self.max_drones = max_drones
         self.color = color
-        self.visited = visited
 
-class Connection:
-    def __init__(self, previous_zone: Zone,
-                 next_zone: Zone,
-                 max_link_capacity: int = 1) -> None:
-        self.previous_zone = previous_zone
-        self.next_zone = next_zone
-        self.max_link_capacity = max_link_capacity
+    def get_connection(self) -> "Zone": ...
+
+#class Connection:
+#    def __init__(self, previous_zone: Zone,
+#                 next_zone: Zone,
+#                 max_link_capacity: int = 1) -> None:
+#        self.previous_zone = previous_zone
+#        self.next_zone = next_zone
+#        self.max_link_capacity = max_link_capacity
